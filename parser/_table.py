aad3ce1d2fe1bc0af1f75d03d2af84de4937df6a
Shohei Wada <pc@wada314.jp>
2015-09-19 02:04:21 +0900

    remove empty columns

--- parser/_table.py
+++ parser/_table.py
@@ -5,16 +5,32 @@ class _Table:
         self.rows = []
 
     def format(self, formatter):
+        self._remove_empty_columns()
         return (formatter.table(True)
                 + u'\n'.join([row.format(formatter) for row in self.rows])
                 + formatter.table(False))
 
+    def _remove_empty_columns(self):
+        col_num = min([len(row.cells) for row in self.rows])
+        is_empty = [True] * col_num
+        for row in self.rows:
+            for i, cell in enumerate(row.cells):
+                is_empty[i] = is_empty[i] and not (cell and cell.text)
+        for row in self.rows:
+            newcells = []
+            for i, cell in enumerate(row.cells):
+                if not is_empty[i]:
+                    newcells.append(cell)
+            row.cells = newcells
+        
+
 class _Row:
-    def __init__(self):
+    def __init__(self, is_header=False):
         self.cells = []
+        self.is_header = is_header
 
     def format(self, formatter):
-        return (formatter.table_row(True)
+        return (formatter.table_row(True, {u'class': u'header'} if self.is_header else {})
                 + u'\n'.join([(cell or _Cell()).format(formatter) for cell in self.cells])
                 + formatter.table_row(False))
         
