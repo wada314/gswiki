966ba4c3ef48652c2cb67ed80de7086cefc26ef8
Shohei Wada <pc@wada314.jp>
2016-03-18 05:00:05 +0900

    Optimize the table title priority table Remove debugging code

--- parser/_table.py
+++ parser/_table.py
@@ -2,7 +2,7 @@
 
 import copy
 
-_JSON_NAME_ORDER = [
+_JSON_NAME_ORDER_LIST = [
     u'名称', u'弾種', u'レベル',
     u'格闘', u'N格', u'上格', u'左格', u'右格', u'下格', u'威力', u'解説',
     u'キャラクターデータ', u'よろけにくさ', u'ジャンプ上昇力', u'空中ダッシュ初速度',
@@ -13,21 +13,22 @@ _JSON_NAME_ORDER = [
     u'チューン', u'メリット', u'デメリット',
     u'攻撃力', u'攻撃力(爆風)', u'散弾攻撃力', u'吸引力', u'回復力', 
     u'連射間隔', u'装填数', u'リロード分子', u'リロード分母', 
-    u'射程距離', u'散弾射程', u'攻撃範囲', u'_分裂数', u'_三点バースト', u'_状態異常',
+    u'射程距離', u'散弾射程', u'攻撃範囲', u'分裂数', u'三点バースト', u'状態異常',
     u'ロックオン時間', u'防御力', u'最低持続時間',
     u'シールド範囲', u'シールド展開時間',
     u'反動ダメージ割合', u'反動ダメージ量',
-    u'弾薬補給割合', u'回復範囲', u'弾薬回復範囲', u'移動時間', u'効果時間', u'_チャージ',
-    u'_備考', u'_サブトリガー', u'_サブウェポン',
+    u'弾薬補給割合', u'回復範囲', u'弾薬回復範囲', u'移動時間', u'効果時間', u'チャージ',
+    u'備考', u'サブトリガー', u'サブウェポン',
 ]
+_JSON_NAME_ORDER_DICT = dict(map(
+    lambda (i, name): (name, i),
+    enumerate(_JSON_NAME_ORDER_LIST)))
+_JSON_NAME_ORDER_TOTAL = len(_JSON_NAME_ORDER_LIST)
 
 def _json_name_priority(name):
     if isinstance(name, unicode):
         name = name.lstrip(u'_')
-    try:
-        return _JSON_NAME_ORDER.index(name)
-    except ValueError:
-        return len(_JSON_NAME_ORDER)
+    return _JSON_NAME_ORDER_DICT.get(name, _JSON_NAME_ORDER_TOTAL)
 
 class _Table:
     def __init__(self):
@@ -35,9 +36,7 @@ class _Table:
 
     def toHtmlTable(self, **kw):
         rows = copy.deepcopy(self.rows)
-        self.debug_print_rows(rows)
         self._remove_empty_columns(rows)
-        self.debug_print_rows(rows)
         html_rows = []
         if kw.get('generate_header', True):
             title_row = self._extract_title_row(rows)
@@ -49,10 +48,6 @@ class _Table:
         for row in rows:
             html_rows.extend(row.toHtmlRows())
         return _HtmlTable(html_rows)
-
-    def debug_print_rows(self, rows):
-        for row in rows:
-            print u', '.join([c.text if c else 'None' for c in row.cells])
 
     @staticmethod
     def _remove_empty_columns(rows):
