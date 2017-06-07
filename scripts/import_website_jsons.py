64b45852b38ff03c1c16d4f0d07c33e38267a231
Shohei Wada <pc@wada314.jp>
2017-06-08 08:41:20 +0900

    発射準備時間

--- scripts/import_website_jsons.py
+++ scripts/import_website_jsons.py
@@ -265,8 +265,6 @@ def processWeapon(j, context, is_sub, dry_run):
     # clenup legacy attrs
     if u'爆発半径' in dst_json:
         del dst_json[u'爆発半径']
-    if u'発射準備時間' in dst_json:
-        del dst_json[u'発射準備時間']
 
     output = (body_pre + u'\n'
               + json_printer.print_json(body_json, indent=2) + u'\n'
